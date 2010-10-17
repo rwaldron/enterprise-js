52381df1d7aa35433e697496f1a5858805c0f606
Ben Truyman <bentruyman@gmail.com>
2010-10-17 11:39:49 -0500

    Added more tippies

--- tips.js
+++ tips.js
@@ -2,12 +2,44 @@ var tips = [];
 
 tips.push({
 	author: 'bentruyman',
-	message: 'Never Using Literals or Lazy Shorthand',
+	message: 'Never Using Literals or <em>Lazy</em> Shorthand',
 	example: [
 		'var integer = new Number(5);',
 		'var myString = new String("Hello World");',
 		'var foo = new Object();',
-		'var arrayOfFoo = new Array(5);'
+		'var arrayOfFoo = new Array(5);',
+		'if (status === new Boolean(true)) {',
+		'	...',
+		'}'
+	]
+});
+
+tips.push({
+	author: 'bentruyman',
+	message: '<em>Self-Documenting</em> Variable Names',
+	example: [
+		'function CatObjectThatInheritsAnimal() {',
+		'	this.yellowFurColor = "yellow";',
+		'	this.animalsAge = 10;',
+		'}',
+		'',
+		'CatObjectThatInheritsAnimal.prototype = new AnimalObjectThatInheritsNothing;'
+	]
+});
+
+tips.push({
+	author: 'bentruyman',
+	message: 'Using Constants to Keep Everything <em>Abstract</em>',
+	example: [
+		'var FALSE = new Boolean(false);',
+		'var TRUE = new Boolean(true);',
+		'var ONE = new Number(1);',
+		'',
+		'function IF_STATEMENT(condition, callback) {',
+		'	if (condition == TRUE) {',
+		'		callback.call(this);',
+		'	}',
+		'}'
 	]
 });
 
