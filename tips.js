bbc9637dc7b65fae351e2a2a22d1eeaf26f40b8b
rwldrn <waldron.rick@gmail.com>
2010-10-18 14:09:02 -0400

    Every var own its own line

--- tips.js
+++ tips.js
@@ -1,69 +1,84 @@
 var tips = [];
 
 tips.push({
-	author: 'bentruyman',
-	message: 'Never Using Literals or <em>Lazy</em> Shorthand',
-	example: [
-		'var integer = new Number(5);',
-		'var myString = new String("Hello World");',
-		'var foo = new Object();',
-		'var arrayOfFoo = new Array(5);',
-		'if (status === new Boolean(true)) {',
-		'	...',
-		'}'
-	]
+  author: 'bentruyman',
+  message: 'Never Using Literals or <em>Lazy</em> Shorthand',
+  example: [
+    'var integer = new Number(5);',
+    'var myString = new String("Hello World");',
+    'var foo = new Object();',
+    'var arrayOfFoo = new Array(5);',
+    'if (status === new Boolean(true)) {',
+    ' ...',
+    '}'
+  ]
 });
 
 tips.push({
-	author: 'bentruyman',
-	message: 'Extremely Descriptive <em>Self-Documenting</em> Variable Names',
-	example: [
-		'function CatObjectThatInheritsAnimal() {',
-		'	this.yellowFurColorString = "yellow";',
-		'	this.animalsAgeInYears = 10;',
-		'}',
-		'',
-		'CatObjectThatInheritsAnimal.prototype = new AnimalObjectThatInheritsNothing;'
-	]
+  author: 'bentruyman',
+  message: 'Extremely Descriptive <em>Self-Documenting</em> Variable Names',
+  example: [
+    'function CatObjectThatInheritsAnimal() {',
+    ' this.yellowFurColorString = "yellow";',
+    ' this.animalsAgeInYears = 10;',
+    '}',
+    '',
+    'CatObjectThatInheritsAnimal.prototype = new AnimalObjectThatInheritsNothing;'
+  ]
 });
 
 tips.push({
-	author: 'bentruyman',
-	message: 'Using Constants to Keep Everything <em>Abstract</em>',
-	example: [
-		'var FALSE = new Boolean(false);',
-		'var TRUE = new Boolean(true);',
-		'var ONE = new Number(1);',
-		'',
-		'function IF_STATEMENT(condition, callback) {',
-		'	if (condition == TRUE) {',
-		'		callback.call(this);',
-		'	}',
-		'}'
-	]
+  author: 'bentruyman',
+  message: 'Using Constants to Keep Everything <em>Abstract</em>',
+  example: [
+    'var FALSE = new Boolean(false);',
+    'var TRUE = new Boolean(true);',
+    'var ONE = new Number(1);',
+    '',
+    'function IF_STATEMENT(condition, callback) {',
+    ' if (condition == TRUE) {',
+    '   callback.call(this);',
+    ' }',
+    '}'
+  ]
 });
 
 tips.push({
-	author: 'bentruyman',
-	message: 'Ensuring Lines Are Terminated With Multiple Semicolons So the JavaSript Parser <em>Really Knows</em> when a Line Is Ending',
-	example: [
-		'var helloWorld = function (message) {',
-		'	if (!message) {',
-		'		message = "Hello World";;;;;;',
-		'	}',
-		'',
-		'	alert(message);;;;;;;;;;;;;;;;;;;',
-		'',
-		'};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;'
-	]
+  author: 'bentruyman',
+  message: 'Ensuring Lines Are Terminated With Multiple Semicolons So the JavaSript Parser <em>Really Knows</em> when a Line Is Ending',
+  example: [
+    'var helloWorld = function (message) {',
+    ' if (!message) {',
+    '   message = "Hello World";;;;;;',
+    ' }',
+    '',
+    ' alert(message);;;;;;;;;;;;;;;;;;;',
+    '',
+    '};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;'
+  ]
 });
 
 tips.push({
-	author: 'bentruyman',
-	message: 'Creating <em>Really</em> Random Numbers',
-	example: [
-		'var reallyRandom = Math.pow(Math.random(), Math.random());'
-	]
+  author: 'bentruyman',
+  message: 'Creating <em>Really</em> Random Numbers',
+  example: [
+    'var reallyRandom = Math.pow(Math.random(), Math.random());'
+  ]
 });
+
+
+tips.push({
+  author: 'rwaldron',
+  message: "Make sure you explicitly declare EVERY VARIABLE on it's own line, that way your team will know they are all variables",
+  example: [
+    'var li = "<li></li>";', 
+    'var div = "<div>"+li+"</div>";', 
+    'var num1 = 4;', 
+    'var num2 = 5;', 
+    'var total = num1 + num2;', 
+    'var rounded = Math.round(total);'
+  ]
+});
+
 
 module.exports = tips;
